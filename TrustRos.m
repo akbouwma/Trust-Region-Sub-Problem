c70a8a01302af54710d3f44841adcab4d6705d41
Caleb Jacobs <cajacobs@mtu.edu>
2021-03-05 00:29:37 -0500

    Fixed a few bugs but the code still seems unstable

--- TrustRos.m
+++ TrustRos.m
@@ -13,13 +13,14 @@ A = matlabFunction(A);
 
 maxIts = 200;       % Maximum iterations allowed
 epsilon = 10^(-8);  % Stopping tolerance
-B = [2 0; 0 0.5];   % B defines the shape of the elliptical trust region
-Delta = 3;          % Initial "radius" of trust region
+B = [1,1; 1,1];     % B defines the shape of the elliptical trust region
+B = B*B';           % Make sure B is SPD
+Delta = 1;          % Initial "radius" of trust region
 x = [0; 0];         % Initial solution guess
 
 % M_tilde pencil defintion
-ML = @(x,y) [-B, A(x,y); A(x,y), -g(x,y)*(g(x,y)')/Delta^2];  % LHS of M-tilde
-MR = [zeros(2,2), -B; -B, zeros(2,2)];                        % RHS of M-tilde
+M0 = @(x,y) [-B, A(x,y); A(x,y), -g(x,y)*(g(x,y)')/Delta^2]; % LHS of M-tilde
+M1 = -[zeros(2,2), B; B, zeros(2,2)];                        % RHS of M-tilde
 
 % Algorithm 5.1 implementation
 for i = 1:maxIts
@@ -32,26 +33,26 @@ for i = 1:maxIts
     
     % Ignore Newton step if it is outside of the trust region
     if ~(BNorm(p0, B) < Delta)
-        p0 = [0;0];
+        p0TEST = false;        % Ignore Newton step, outside of region
+    else
+        p0TEST = true;         % Allow Newton step this iteration
     end
     
     % Find the rightmost eigenvalue/vector of the pencil M_tilde
-    [y, lambda] = eigs(ML(x(1),x(2)), MR, 1, 'largestreal');
-    y = real(y);
-    y1 = y(1:2);
-    y2 = y(3:4);
-    
-    % Compute boundary step from rightmost eigenvector
-    p1 = -sign(g(x(1),x(2))' * y2) * Delta * y1 / BNorm(y1, B);
+    [y, lambda] = eigs(M0(x(1),x(2)), M1, 1, 'largestreal');
+    p1 = y(3:4);
+    p1 = p1/BNorm(p1,B)*Delta;
     
     % Choose the best step between Newton and boundary steps
-    if (f(x(1) + p0(1), x(2) + p0(2)) < f(x(1) + p1(1), x(2) + p1(2)))
+    if (p0TEST && f(x(1) + p0(1), x(2) + p0(2)) < f(x(1) + p1(1), x(2) + p1(2)))
         x = x + p0;
     else
         x = x + p1;
+        p0TEST = true;
     end
 end
 
+i       % Iterations used
 x       % Minimizer of f
 
 
